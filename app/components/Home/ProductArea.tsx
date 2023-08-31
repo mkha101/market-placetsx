f468648817c944e6a449090df0803060bdb9d097
Muzamil Khan <92785420+mkha101@users.noreply.github.com>
2023-08-31 16:04:32 -0400

    top brand circle image fixed

--- app/components/Home/ProductArea.tsx
+++ app/components/Home/ProductArea.tsx
@@ -17,10 +17,10 @@ export const ProductArea = () => {
       <div className="flex flex-col justify-center items-center">
         <h1 className="text-xl sm:mt-10 mt-5">Top Selling Brands</h1>
         <div className="flex flex-row text-black gap-6 mt-16">
-          <TopBrandsCircle imageSrc="/Apple.webp" brandName="Apple" />
+          <TopBrandsCircle imageSrc="/applee.png" brandName="Apple" />
           <TopBrandsCircle imageSrc="/Nike.jpeg" brandName="Nike" />
           <TopBrandsCircle imageSrc="/Sony.png" brandName="Sony" />
-          <TopBrandsCircle imageSrc="/Apple.webp" brandName="Apple" />
+          <TopBrandsCircle imageSrc="/adidas.png" brandName="Adidas" />
         </div>
       </div>
       <div className="flex flex-col justify-center items-center">
