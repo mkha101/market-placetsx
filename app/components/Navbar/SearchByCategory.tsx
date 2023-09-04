3dbfa75f541871b64827cbabd245a732ca7b8192
Muzamil Khan <92785420+mkha101@users.noreply.github.com>
2023-09-04 17:45:22 -0400

    backgroud gradient @media for mobile

--- app/components/Navbar/SearchByCategory.tsx
+++ app/components/Navbar/SearchByCategory.tsx
@@ -12,11 +12,11 @@ const SearchByCategory = () => {
     <div>
       <div className="hidden sm:flex">
         <Select>
-          <SelectTrigger className="w-[225px] p-[18px] text-[#E0AD9A] bg-white cursor-pointer rounded-full  rounded-r-none ">
+          <SelectTrigger className="w-[225px] p-[18px]  text-[#E0AD9A] bg-white cursor-pointer rounded-full  rounded-r-none ">
             <SelectValue placeholder="Search by Category" />
           </SelectTrigger>
 
-          <SelectContent>
+          <SelectContent className="">
             <SelectItem className="cursor-pointer" value="Clothing">
               Clothing
             </SelectItem>
