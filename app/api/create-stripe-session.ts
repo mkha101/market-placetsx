8b3d6bcb68798377f6086dcb1d11fe854b18b97b
Muzamil Khan <92785420+mkha101@users.noreply.github.com>
2024-11-09 17:16:36 -0500

    type error fixed

--- app/api/create-stripe-session.ts
+++ app/api/create-stripe-session.ts
@@ -1,5 +1,5 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async (req: Request, res: Response) => {
-  const { item } = req.body;
+  const { item } = req.body as unknown as { item: any }; // Replace `any` with the actual type if you know it
 };
